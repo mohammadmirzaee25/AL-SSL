8931ed9c89cbd4f08b9fc4fb3844f10ecd2eeff9
mohammadmirzaee25 <94175691+mohammadmirzaee25@users.noreply.github.com>
2024-08-24 16:05:04 +0330

    Update config.py

--- data/config.py
+++ data/config.py
@@ -14,7 +14,7 @@ MEANS = (104, 117, 123)
 voc300 = {
     'num_classes': 21,
     'lr_steps': (80000, 100000, 120000),
-    'max_iter': 15,
+    'max_iter': 5000,
     'feature_maps': [38, 19, 10, 5, 3, 1],
     'min_dim': 300,
     'steps': [8, 16, 32, 64, 100, 300],
@@ -28,7 +28,7 @@ voc300 = {
 voc512 = {
     'num_classes': 21,
     'lr_steps': (80000, 100000, 120000),
-    'max_iter': 15,
+    'max_iter': 5000,
     'feature_maps': [64, 32, 16, 8, 4, 2, 1],
     'min_dim': 512,
     'steps': [8, 16, 32, 64, 128, 256, 512],
@@ -44,7 +44,7 @@ voc512 = {
 coco = {
     'num_classes': 81,
     'lr_steps': (80000, 100000, 120000),
-    'max_iter': 15,
+    'max_iter': 5000,
     'feature_maps': [38, 19, 10, 5, 3, 1],
     'min_dim': 300,
     'steps': [8, 16, 32, 64, 100, 300],
